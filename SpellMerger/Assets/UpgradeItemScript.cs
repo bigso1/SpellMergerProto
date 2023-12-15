531a3b6a10d56d3f64d663f69eaf8ce496e43787
Romaing <r.cappelletti@rubika-edu.com>
2023-12-15 11:48:07 +0100

    UpgradeItem

---
+++ SpellMerger/Assets/UpgradeItemScript.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeItemScript : MonoBehaviour
+{
+    [SerializeField] private GameObject uiToSpawn;
+
+    private bool isClaimable;
+    // Start is called before the first frame update
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(!other.CompareTag("Player")) return;
+        uiToSpawn.SetActive(true);
+        isClaimable = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(!other.CompareTag("Player")) return;
+        uiToSpawn.SetActive(false);
+        isClaimable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isClaimable) return;
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            FindObjectOfType<Controler>().breathUnlocked = true;
+            Destroy(gameObject);
+        }
+    }
+}
